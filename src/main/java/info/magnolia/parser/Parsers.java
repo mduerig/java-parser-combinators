5d94abc83bfa51aef604c6f3a5b975ae08bf2dbd
Michael Dürig <michid@gmail.com>
2021-09-19 12:44:01 +0200

    Fix non assosiative reduction in integer()

--- src/main/java/info/magnolia/parser/Parsers.java
+++ src/main/java/info/magnolia/parser/Parsers.java
@@ -4,11 +4,15 @@ import static info.magnolia.parser.Parser.failure;
 import static info.magnolia.parser.Parser.none;
 import static info.magnolia.parser.Parser.result;
 import static info.magnolia.parser.Parser.success;
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.reducing;
 import static java.util.stream.Stream.concat;
 
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 public final class Parsers {
@@ -63,10 +67,19 @@ public final class Parsers {
     }
 
     public static Parser<Integer> integer() {
+        // Left associative reduction aka foldLeft. See https://bugs.openjdk.java.net/browse/JDK-8133680
+        Collector<Integer, ?, Integer> toInteger = collectingAndThen(
+            reducing(
+                Function.<Integer>identity(),
+                d -> n -> n * 10 + d,
+                Function::andThen),
+            endo -> endo.apply(0));
+
         return digit()
             .some()
             .map(digits ->
-                digits.reduce(0, (n, d) -> n * 10 + d));
+                digits.collect(toInteger));
+                // digits.reduce(0, (n, d) -> n * 10 + d));  not associative -> fails on parallel streams
     }
 
     public static Parser<String> chars(Predicate<Character> predicate) {
